aa48ffa6298d896b14b197b417d1ee15d966dbed
Luiz <luizfellipe230@gmail.com>
2025-02-10 15:09:21 -0300

    Loading Meal

---
+++ components/by-page/diets/selectedDiet/Refs/DietMealLoading.tsx
@@ -0,0 +1,11 @@
+export default function DietMealLoading() {
+  return (
+    <div>
+      <div className='border-grey10 border-1 rounded-lg flex flex-col w-full relative h-40'>
+        <div className='c-animated-background'>
+          <div className='c-animated-inside'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
